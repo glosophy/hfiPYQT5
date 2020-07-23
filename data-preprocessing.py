c21fefe5b1beb0f2b939b35793e82dfd6aa2b3ca
guillermina <suttersg@gmail.com>
2020-07-23 15:41:51 -0400

    Data preprocessing and csv file

---
+++ data-preprocessing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import os
+
+cwd = os.getcwd()
+print('Current working directory:', cwd)
+
+#Load csv
+hfi = pd.read_csv(cwd + '/hfi2019.csv')
+print(hfi.head())
+print(hfi.dtypes)
+
+#Clean '-' values and coerce score columns to float
+hfi = hfi.replace('-', np.nan)
+hfi = hfi.replace(' ', np.nan)
+hfi.iloc[:,4:] = hfi.iloc[:,4:].apply(pd.to_numeric)
